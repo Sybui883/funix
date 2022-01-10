cf0390f92f1d5f192a9b5385d131099d307ad57f
Sybui883 <sybui883@gmail.com>
2022-01-10 10:36:00 +0700

    update code 5

---
+++ src/StaffListComponent/Colum.js
@@ -0,0 +1,11 @@
+function Colum() {
+    return (
+        <select>
+            <option>1 col</option>
+            <option>2 col</option>
+            <option>3 col</option>
+        </select>
+    )
+}
+
+export default Colum()
