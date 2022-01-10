b9e2ee6b9a3abd7f73efae6a0de4b3b574e9798b
Sybui883 <sybui883@gmail.com>
2022-01-10 11:38:19 +0700

    update code 8

---
+++ src/StaffListComponent/Col.js
@@ -0,0 +1,11 @@
+function Col() {
+    return (
+        <div id="col">
+        <button>1-Col</button>
+        <button>2-Col</button>
+        <button>3-Col</button>
+        </div>
+    )
+
+}
+export default Col;
