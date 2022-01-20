8406879fe95f60f3ab56234e56cb5288432e8c9d
Sybui883 <sybui883@gmail.com>
2022-01-20 15:20:25 +0700

    update code 1

---
+++ src/StaffListComponent/StaffList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom';
+import { STAFFS } from 'staffs'
+
+function StaffList() {
+    constructor(props) {
+        super(props);
+    }
+    return (
+
+    )
+}
+
+export default StaffList;f
