fd516de9eeea1be84f0e8e55da9e07db2d92ee65
Sybui883 <sybui883@gmail.com>
2022-01-25 12:27:11 +0700

    update code 13

--- src/StaffListComponent/ChiTietNV.js
+++ src/StaffListComponent/ChiTietNV.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import dateFormat from "dateFomat";
 import { STAFFS } from "./staffs";
 
 function ChiTietNV() {
   return (
-        <div key={STAFFS.id} id="chitietNV">
-          <img src={STAFFS.image} alt={STAFFS.name} />
-          <h1>Họ và tên: {STAFFS.name}</h1>
-          <p>Ngày sinh: {STAFFS.doB}</p>
-          <p>Ngày vào công ty: {STAFFS.startDate}</p>
-          <p>Phòng ban: {STAFFS.department.name}</p>
-          <p>Số ngày nghỉ còn lại: {STAFFS.annualLeave}</p>
-          <p>Số ngày đã làm thêm: {STAFFS.overTime}</p>
-        </div>
-      )
-};
-
+    <div key={STAFFS.id} id="chitietNV">
+      <img src={STAFFS.image} alt={STAFFS.name} />
+      <h1>Họ và tên: {STAFFS.name}</h1>
+      <p>Ngày sinh: {dateFormat(STAFFS.doB, "dd/mm/yyyy")}</p>
+      <p>Ngày vào công ty: {dateFormat(STAFFS.startDate, "dd/mm/yyyy")}</p>
+      <p>Phòng ban: {STAFFS.department.name}</p>
+      <p>Số ngày nghỉ còn lại: {STAFFS.annualLeave}</p>
+      <p>Số ngày đã làm thêm: {STAFFS.overTime}</p>
+    </div>
+  );
+}
 
 export default ChiTietNV;
