b4087fba8d2dba52848197912215d32a0a329180
Sybui883 <sybui883@gmail.com>
2022-02-22 10:23:29 +0700

    Update Code 1

--- src/StaffListComponent/NhanVien.js
+++ src/StaffListComponent/NhanVien.js
@@ -1,32 +1,60 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import React, { Component } from "react";
+import { ReactDOM } from "react-dom";
+import { Link } from "react-router-dom";
 import { STAFFS } from "./staffs";
 import "./css/NhanVien.css";
 import { Card, CardImg, CardTitle } from "reactstrap";
+import { Modal } from "bootstrap";
+import ThemNhanVien from "./ThemNhanVien";
+import FormTimKiem from "./FormTimKiem";
 
-function NhanVien() {
-  return (
-    <div id="home">
-      <h1>Nhân viên</h1>
-      <hr />
-      {STAFFS.map((STAFFS) => {
-        return (
-          <div key={STAFFS.id} id="nhanVien">
-            <Card>
+class NhanVien extends Component {
+  state = {
+    value: "",
+  };
+
+  // Thay đổi giá trị input
+  handleChangeValue = (newChange) => {
+    this.setState({
+      value: newChange,
+    });
+  };
+
+  //Modal thêm nhân viên
+  themNhanVien = () => {};
+
+  render() {
+    return (
+      <div id="home">
+        <div className="row">
+          <h1 className="col-4">Nhân viên</h1>
+          <div className="col-2">
+            <ThemNhanVien />
+          </div>
+          <div className="col-6">
+            <FormTimKiem />
+          </div>
+        </div>
+        <hr />
+        {STAFFS.map((STAFFS) => {
+          return (
+            <div key={STAFFS.id} id="nhanVien">
+              <Card>
                 <Link to={`/nhan-vien/${STAFFS.id}`}>
                   <CardImg
                     className="card-img-top w-150 h-150"
                     src={STAFFS.image}
                     alt={STAFFS.name}
                   ></CardImg>
-                    <CardTitle className="card-text"> {STAFFS.name}</CardTitle>
+                  <CardTitle className="card-text"> {STAFFS.name}</CardTitle>
                 </Link>
-            </Card>
-          </div>
-        );
-      })}
-    </div>
-  );
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default NhanVien;
---
+++ src/StaffListComponent/ThemNhanVien.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <div>
+        {" "}
+        <div>
+          {/* Button trigger modal */}
+          <button
+          style={{width: '50px', margin: '10px 50px'}}
+            type="button"
+            className="btn btn-dark btn-lg"
+            data-toggle="modal"
+            data-target="#modelId"
+          >
+            +
+          </button>
+          {/* Modal */}
+          <div
+            className="modal fade"
+            id="modelId"
+            tabIndex={-1}
+            role="dialog"
+            aria-labelledby="modelTitleId"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Modal title</h5>
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">×</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <div className="container-fluid">Add rows here</div>
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" className="btn btn-primary">
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
