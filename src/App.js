a5df8f1782cbcbfd3fa82e6ae4e68f7da327e568
Sybui883 <sybui883@gmail.com>
2022-02-23 15:54:06 +0700

    delete and wrinting code with Class Component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import { Route, Routes, Switch } from "react-router-dom";
-import "./StaffListComponent/css/App.css";
-import NhanVien from "./StaffListComponent/NhanVien";
-import BangLuong from "./StaffListComponent/BangLuong";
-import PhongBan from "./StaffListComponent/PhongBan";
-import Home from "./StaffListComponent/Home";
-import ChiTietNV from "./StaffListComponent/ChiTietNV";
-import Header from "./StaffListComponent/Header";
-import Footer from "./StaffListComponent/Footer";
-import NotFound from "./NotFound";
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App container-fluid">
-      <nav>
-        <Header />
-      </nav>
-      <Routes>
-          <Route path="/" element={<NhanVien />} />
-            <Route path="/nhan-vien/:staffId" element={<ChiTietNV />} />
-         
-          <Route path="phong-ban" element={<PhongBan />} />
-          <Route path="bang-luong" element={<BangLuong />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/nhan-vien/*" element={<NotFound />} />
-        </Routes>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
-
-export default App;
